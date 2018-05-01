bfa9427797bab6e5eb4cc0d73c6a5e44ffe680e6
Hadas Ben-Tsur <hadasbt@gmail.com>
2018-05-01 12:31:24 -0400

    js css tweaks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(function() {
     // $('.mainPage').css("display", "flex");
-    formDropdown();
+    // formDropdown();
     apiCall();
     // setListenersForprofilePage();
     // setListenersForWhosWhoPage();
@@ -10,11 +10,32 @@ function apiCall() {
     var marvelAPI = 'https://gateway.marvel.com/v1/public/comics';
     $.getJSON(marvelAPI, {
         apikey: 'ab730e7076afd4c1be7f21cabdc8b507'
-    });
+    })
+    .done(function(response){
+        console.log("Lets see the response " + response);
+    })
+}
+//      .done(function( response ) {
+//       var results = response.data.results;
+//       debugger;
+//       var resultsLen = results.length;
+//       var output = '<ul>'; 
+      
+//       for(var i=0; i<resultsLen; i++){
+//         if(results[i].images.length > 0) {
+//           var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
+//           output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
+//         }
+//       }  
+//       output += '</ul>'
+//       $('#results').append(output);
+//   });
+   
+// };
 
-    function formDropdown(#teams) {
-        let params = {
-            part: 'events'
-            maxResults: 7
-        }
-    };
+    // function formDropdown(#teams) {
+    //     let params = {
+    //         part: 'events'
+    //         maxResults: 7
+    //     }
+    // };
