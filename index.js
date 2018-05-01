fd426a6e05fad489c1bb79c3e2eefa31452d24d7
Hadas Ben-Tsur <hadasbt@gmail.com>
2018-04-30 20:39:51 -0400

    Starting JS + API work

---
+++ index.js
@@ -0,0 +1,12 @@
+$(function(){
+var marvelAPI = 'https://gateway.marvel.com/v1/public/comics';
+$.getJSON( marvelAPI, {
+    apikey: 'ab730e7076afd4c1be7f21cabdc8b507'
+  });
+
+function formDropdown(#teams){
+  let params= {
+    part: 'events'
+    maxResults:7
+  }
+}
