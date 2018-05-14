bab98f5ac664af7480177b4cff3ca7f148430078
Hadas Ben-Tsur <hadasbt@gmail.com>
2018-05-14 15:01:05 -0400

    added JS for profile page

--- index.js
+++ index.js
@@ -88,25 +88,76 @@ function populateMemberProfile(data) {
 
     $(newUL).appendTo(ProfPage);
 
-    // console.log(data[i].description);
-    const appendText = $("<li class='newLI'>" + "<span>" + teamMemberName + "</span>" + description + "<button onClick='showProjectPage("+data[i].series.collectionURI+")'>Projects</button></li>").appendTo(newUL);
+    console.log(data[i].id);
+    const appendText = $("<li class='newLI'>" + "<span>" + teamMemberName + "</span>" + description + "<button onClick='populateProjectPage("+data[i].series.collectionURI+")'>Projects</button></li>").appendTo(newUL);
    
     // appendText.appendTo(Members);
        }
 }
 
-// function showProjectPage() {
-//   $('#teams').on('change', function() {
-//     // apiCallSeries();
-//     // ^^ create this!
-//     populateProjectPage();
-//   }
-// }
 
-// function populateProjectPage() {
-//   const projects = $('.projectHeader');
-//   const projectPhotos = $('.largerImageBox');
-// }
+// grab characterID so I can then use the ID to grab the Series
+function getCharacterId() {
+        $(".profilePage").empty();
+        // console.log(this.value);
+        const characterID = this.value;
+        apiCallSeries(eventID);
+};
+
+// search for Series using API
+function createSeriesSearchString(characterID, limit, key, modified) {
+    return 'https://gateway.marvel.com:443/v1/public/characters/' + characterID + '/series?orderBy=' + modified + '&limit=' + limit + '&apikey=' + key;
+}
+
+// actually call the API that will grab the Series/Projects
+function apiCallSeries(characterID) {
+    const marvelAPIS = createSeriesSearchString(characterID, 7, 'ab730e7076afd4c1be7f21cabdc8b507', 'modified');
+    let profileDescription = "Hmm. Looks like your team member hasn't had a lot going on. Maybe they like to keep to themselves. Reach out to your team member to find out if they're looking for a chance to get more involved or prefer to work on their own private projects.";
+
+    $.getJSON(marvelAPIS, function(result) {
+        populateProjectProfile(result.data.results);
+    }).done(function(response) {
+        if (result.data.results > 1) {
+      projectDescription = result.data.results;
+    }
+    })
+};
+
+// Create the member profiles via API info
+function populateProjectProfile(data) {
+    
+    // all the naming
+    const projPage = $('.projectsPage');
+    const projHead = $('.projectHeader');
+    const projectPhotos = $('.largerImageBox');
+    const Projects = $('.projectProfile');
+    let newUL = document.createElement("UL");
+    newUL.className="projectProfile";
+
+// loop start
+    for (let i = 0; i < data.length; i++) { 
+
+// loop specific naming
+    const profImageSRC = data[i].thumbnail.path + "." + data[i].thumbnail.extension;
+    let projectName = data[i].name;
+
+// filler text for profiles without descriptions
+    let description = "It seems like your team members didn't get a chance to write a description about their project. Reach out to your supervisor to find out more.";
+    if (data[i].description.length > 1) {
+      description = data[i].description;
+    }
+
+// appendages
+    const appendProfileImage = $("<li class='largerImageBox'> <img src='" + profImageSRC + "'> </li>").appendTo(newUL);
+
+    $("appendProfileImage").appendTo(projectPhotos); 
+
+    $(newUL).appendTo(profPage);
+       }
+}
+
+
+
 
 //^^ check out the series link and fixed. Button with on click that calls some javascript, takes eventID and collectionURI as params. 
 
